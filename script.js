d2bfdc66f34226df7eb7615ed006f4cd5c29c054
Fernando Espinosa <67436442+FerEspinosa@users.noreply.github.com>
2024-10-24 20:43:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,25 +17,7 @@ for (const navLink of navLinks) {
 	});
 }
 
-const navLinks = document.querySelectorAll('nav ul li a');
-
-for (const navLink of navLinks) {
-	navLink.addEventListener('click', function(e) {
-		const hrefValue = this.getAttribute('href');
-
-		// Si el enlace es un ancla que apunta a una sección interna
-		if (hrefValue.startsWith('#')) {
-			e.preventDefault();  // Evita la acción predeterminada solo para anclas internas
-			const section = document.querySelector(hrefValue);
-			if (section) {
-				const sectionTop = section.offsetTop;
-				window.scrollTo({ top: sectionTop, behavior: 'smooth' });
-			}
-		} 
-		// Si es un enlace externo (como un cambio de página), el comportamiento por defecto es permitido
-	});
-}
-
+// Code for showing the message box
 const image = document.getElementById('myImage');
 const messageBox = document.getElementById('messageBox');
 
@@ -51,4 +33,5 @@ if (image && messageBox) {
       }, 500); 
     }, 5000);
   });
-}
+}			
+
